ea2a65ff81ff9d21d0fc60bab63c0d088dd4ed8d
fakorede <abiolafakorede@gmail.com>
2020-02-20 22:26:28 +0100

    created a listing page and displaying products from api

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -1,15 +1,90 @@
 <template>
-  <div class="container">
-    <a
-      href="#"
-      class="a-button-buy-again"
-    >Buy Now</a>
-  </div>
+  <main>
+    <div class="a-spacing-large"></div>
+    <div class="container-fluid browsing-history">
+      <div class="row">
+        <div class="col-sm-8 col-8">
+          <h1 class="a-size-large a-spacing-none a-text-normal">
+            All Products
+          </h1>
+          <div class="a-spacing-large"></div>
+          <!-- Buttons -->
+          <a href="#" class="a-button-buy-again">Add a new product</a>
+          <a href="#" class="a-button-history margin-right-10"
+            >Add a new category</a
+          >
+          <a href="#" class="a-button-history margin-right-10"
+            >Add a new owner</a
+          >
+          <!-- Listings -->
+        </div>
+      </div>
+    </div>
+
+    <div class="a-spacing-large"></div>
+
+    <!-- Listings -->
+    <div class="container-fluid browsing-history">
+      <div class="row">
+        <div
+          v-for="(product, index) in products"
+          :key="product._id"
+          class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb"
+        >
+          <div class="history-box">
+            <!-- product image -->
+            <a href="#" class="a-link-normal">
+              <img :src="product.photo" class="img-fluid" />
+            </a>
+            <!-- product title -->
+            <div class="a-spacing-top-base asin-title">
+              <span class="a-text-normal">
+                <div class="p13n-sc-truncated">{{ product.title }}</div>
+              </span>
+            </div>
+            <!-- product ratings -->
+            <div class="a-row">
+              <a href="#">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </a>
+              <span class="a-letter-space"></span>
+              <span class="a-color-tertiary a-size-small asin-reviews"
+                >(5678)</span
+              >
+            </div>
+            <!-- product price -->
+            <div class="a-row">
+              <span class="a-size-base a-color-price">
+                <span class="p13n-sc-price">${{ product.price }}</span>
+              </span>
+            </div>
+            <!-- product buttons -->
+            <div class="a-row">
+              <a href="#" class="a-button-history margin-right-10">Update</a>
+              <a href="#" class="a-button-history margin-right-10">Delete</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-export default {};
+export default {
+  async asyncData({ $axios }) {
+    try {
+      let response = await $axios.get("http://localhost:4005/api/v1/products");
+      return {
+        products: response.data.products
+      };
+    } catch (err) {}
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
