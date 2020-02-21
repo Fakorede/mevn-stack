6ca033c7f37fc4b834d766572b85630e6c805590
fakorede <abiolafakorede@gmail.com>
2020-02-21 22:46:15 +0100

    setup client nuxt app and configured static files

---
+++ client/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <nuxt-link to="/products" class="a-button-buy-again"
+      >Add a new product</nuxt-link
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
