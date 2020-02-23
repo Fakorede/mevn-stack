8566fd03681f98e406c3653f389c5ee6e2a11646
fakorede <abiolafakorede@gmail.com>
2020-02-23 02:06:49 +0100

    product page

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -17,4 +17,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+body {
+  font-size: 13px;
+  line-height: 19px;
+  color: #111;
+}
+</style>
---
+++ client/pages/products/_id.vue
@@ -0,0 +1,85 @@
+<template>
+  <main>
+    <!-- breadcrumbs -->
+    <div class="a-spacing-top-medium"></div>
+    <div class="container-fluid">
+      <div class="wayfinding-breadcrumbs-container">
+        <ul class="a-unordered-list a-horizontal a-size-small">
+          <li>
+            <span class="a-list-item">
+              <a href="#" class="a-link-normal a-color-tertiary">
+                Category
+              </a>
+            </span>
+          </li>
+          <li>
+            <span class="a-list-item">></span>
+          </li>
+          <li>
+            <span class="a-list-item">
+              <a href="#" class="a-link-normal a-color-tertiary">
+                Product Title
+              </a>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <div class="dp-container">
+        <div class="row">
+          <div class="col-lg-3 col-md-3 col-sm-4">
+            <div class="leftCol">
+              <!-- image -->
+              <div class="imgBlock">
+                <div class="eBooksimg">
+                  <img src="/img/featuredProduct.jpg" class="img-fluid" />
+                </div>
+              </div>
+              <!-- follow author -->
+              <div class="authorFollow">
+                <hr class="a-divider-normal" />
+                <h1 class="authorFollowHeading">Follow the Author</h1>
+                <div class="a-spacing-top-small">
+                  <div class="row">
+                      <!-- author's image -->
+                    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
+                      <div class="smallAuthorImageContainer">
+                        <a href="#">
+                          <img
+                            src="/img/featuredProduct.jpg"
+                            class="img-fluid"
+                          />
+                        </a>
+                      </div>
+                    </div>
+                     <!-- author's name -->
+                    <div class="col-xl-4 col-lg-3 col-md-3 col-sm-3 col-3">
+                      <div class="authorNameCol">
+                        <a href="#">
+                          Walter Philips
+                        </a>
+                      </div>
+                    </div>
+                     <!-- author's follow btn -->
+                    <div class="col-xl-5 col-lg-6 col-md-6 col-sm-6 col-6">
+                      <div class="authorBtn mt-2">
+                        <a href="#">
+                          <span class="btnFollow">
+                            <span class="a-btn-inner">
+                              <button class="a-btn-text">+ Follow</button>
+                            </span>
+                          </span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
