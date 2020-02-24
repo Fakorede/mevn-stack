150921d36bdfcafaab8d5b921af5d141a57a40fb
fakorede <abiolafakorede@gmail.com>
2020-02-24 16:02:40 +0100

    product page - description section

--- client/pages/products/_id.vue
+++ client/pages/products/_id.vue
@@ -7,7 +7,10 @@
         <ul class="a-unordered-list a-horizontal a-size-small">
           <li>
             <span class="a-list-item">
-              <a href="#" class="a-link-normal a-color-tertiary">
+              <a
+                href="#"
+                class="a-link-normal a-color-tertiary"
+              >
                 Category
               </a>
             </span>
@@ -17,7 +20,10 @@
           </li>
           <li>
             <span class="a-list-item">
-              <a href="#" class="a-link-normal a-color-tertiary">
+              <a
+                href="#"
+                class="a-link-normal a-color-tertiary"
+              >
                 Product Title
               </a>
             </span>
@@ -28,12 +34,16 @@
     <div class="container-fluid">
       <div class="dp-container">
         <div class="row">
+          <!-- product image and author's section -->
           <div class="col-lg-3 col-md-3 col-sm-4">
             <div class="leftCol">
               <!-- image -->
               <div class="imgBlock">
                 <div class="eBooksimg">
-                  <img src="/img/featuredProduct.jpg" class="img-fluid" />
+                  <img
+                    src="/img/featuredProduct.jpg"
+                    class="img-fluid"
+                  />
                 </div>
               </div>
               <!-- follow author -->
@@ -42,7 +52,7 @@
                 <h1 class="authorFollowHeading">Follow the Author</h1>
                 <div class="a-spacing-top-small">
                   <div class="row">
-                      <!-- author's image -->
+                    <!-- author's image -->
                     <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3">
                       <div class="smallAuthorImageContainer">
                         <a href="#">
@@ -53,7 +63,7 @@
                         </a>
                       </div>
                     </div>
-                     <!-- author's name -->
+                    <!-- author's name -->
                     <div class="col-xl-4 col-lg-3 col-md-3 col-sm-3 col-3">
                       <div class="authorNameCol">
                         <a href="#">
@@ -61,7 +71,7 @@
                         </a>
                       </div>
                     </div>
-                     <!-- author's follow btn -->
+                    <!-- author's follow btn -->
                     <div class="col-xl-5 col-lg-6 col-md-6 col-sm-6 col-6">
                       <div class="authorBtn mt-2">
                         <a href="#">
@@ -71,6 +81,158 @@
                             </span>
                           </span>
                         </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- description -->
+          <div class="col-lg-6 col-md-6 col-sm-8">
+            <div class="centerCol">
+              <!-- product title -->
+              <div class="titleDiv">
+                <h1 class="productTitle">
+                  <span class="largeTitle">Harry Potter</span>
+                  <span class="smallTitle">Paperback</span>
+                </h1>
+              </div>
+              <!-- author's name -->
+              <div class="bylineinfo">
+                by
+                <a
+                  href="#"
+                  class="authorName"
+                >Walter Steveson
+                  <i
+                    class="fas fa-chevron-down"
+                    style="font-size: 8px !important;color:#555 !important;"
+                  ></i>
+                </a> (Author)
+              </div>
+              <div class="reviewGroup">
+                <hr style="margin-top: 10px;">
+                <!-- dummy data -->
+                <div class="mediaMatrix">
+                  <div class="formats">
+                    <a
+                      href="#"
+                      class="link-expander"
+                    >
+                      >
+                      <span class="tnnShowPrompt">See all 18 formats and editions</span>
+                    </a>
+                    <ul>
+                      <!-- kindle -->
+                      <li
+                        class="swatchElement"
+                        style="width: 117px;"
+                      >
+                        <span class="a-list-item">
+                          <span class="a-button-toggle">
+                            <span class="a-button-inner">
+                              <a
+                                href="#"
+                                class="a-button-text"
+                              >
+                                <span>Kindle</span>
+                                <br>
+                                <span class="a-color-secondary">-</span>
+                              </a>
+                            </span>
+                          </span>
+                          <span class="tnn-olp-links"></span>
+                          <span class="tnn-olp-links">
+                            <a
+                              href="#"
+                              class="a-size-mini"
+                            >
+                              <span class="kcAppBox">
+                                <span class="a-declarative">
+                                  Read with Our
+                                  <span class="a-text-bold">Free App</span>
+                                </span>
+                              </span>
+                            </a>
+                          </span>
+                        </span>
+                      </li>
+                      <!-- audible -->
+                      <li
+                        class="swatchElement"
+                        style="width: 117px;"
+                      >
+                        <span class="a-list-item">
+                          <span class="a-button-toggle">
+                            <span class="a-button-inner">
+                              <a
+                                href="#"
+                                class="a-button-text"
+                              >
+                                <span>
+                                  <img
+                                    src="/img/audibleLogo.png"
+                                    style="width: 20px;"
+                                    class="img-fluid"
+                                  >
+                                  Audible</span>
+                                <br>
+                                <span class="a-color-secondary">-</span>
+                              </a>
+                            </span>
+                          </span>
+                          <span class="tnn-olp-links"></span>
+                          <span class="tnn-olp-links">
+                            <a
+                              href="#"
+                              class="a-size-mini"
+                            >
+                              <span class="kcAppBox">
+                                <span class="a-declarative">
+                                  <span class="a-text-bold">Free App</span> with your Audible Trial
+                                </span>
+                              </span>
+                            </a>
+                          </span>
+                        </span>
+                      </li>
+                      <!-- paperback -->
+                      <li
+                        class="swatchElement"
+                        style="width: 117px;"
+                      >
+                        <span class="a-list-item">
+                          <span class="a-button-toggle">
+                            <span class="a-button-inner">
+                              <a
+                                href="#"
+                                class="a-button-text"
+                              >
+                                <span>Paperback</span>
+                                <br>
+                                <span class="a-color-secondary">-</span>
+                              </a>
+                            </span>
+                          </span>
+                        </span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <!-- description -->
+                <div class="bookDescription">
+                  <div class="bookDescriptionInner">
+                    Lorem ipsum dolor sit amet.
+                  </div>
+                </div>
+                <!-- product specification -->
+                <div class="aboutEbooksFeature">
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-4 mb-1">
+                      <div class="a-declarative">
+                        Length: <span><a href="#">386 pages <i class="a-icon a-icon-popover"></i></a></span>
                       </div>
                     </div>
                   </div>
