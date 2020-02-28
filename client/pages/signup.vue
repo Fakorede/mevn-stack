00ce60d6df807f3ca8d1bacd1426428cc66ae69b
fakorede <abiolafakorede@gmail.com>
2020-02-28 11:52:42 +0100

    signup page

---
+++ client/pages/signup.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="registerPage">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4"></div>
+        <div class="col-sm-4">
+          <div class="text-center">
+            <a href="#">
+              <img src="/img/logo-black.png" />
+            </a>
+          </div>
+
+          <form class="mt-4">
+            <div class="a-box a-spacing-extra-large">
+              <div class="a-box-inner">
+                <h1 class="a-spacing-small">Create Account</h1>
+                <!-- name -->
+                <div class="a-row a-spacing-base">
+                  <label for="ap_customer_name" class="a-form-label"
+                    >Your name</label
+                  >
+                  <input
+                    type="text"
+                    id="ap_customer_name"
+                    class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info"
+                  />
+                </div>
+                <!-- email -->
+                <div class="a-row a-spacing-base">
+                  <label for="ap_customer_name" class="a-form-label"
+                    >Email</label
+                  >
+                  <input
+                    type="email"
+                    id="ap_customer_name"
+                    class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info"
+                  />
+                </div>
+                <!-- password -->
+                <div class="a-row a-spacing-base">
+                  <label for="ap_customer_name" class="a-form-label"
+                    >Password</label
+                  >
+                  <input
+                    type="password"
+                    id="ap_customer_name"
+                    class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info"
+                  />
+                  <div class="a-alert-container">
+                    <div class="a-alert-content">
+                      Password must be at least 6 characters
+                    </div>
+                  </div>
+                </div>
+                <!-- button -->
+                <div class="a-row a-spacing-extra-large mb-4">
+                  <span class="a-button-primary">
+                    <span class="a-button-inner">
+                      <span class="a-button-text"
+                        >Create your Amazon account</span
+                      >
+                    </span>
+                  </span>
+                  <div class="a-row a-spacing-top-medium a-size-small">
+                    <b>
+                      By creating an account, you agree to Amazon's
+                      <a href="#">Conditions of use</a> and
+                      <a href="#">Privacy notice</a>
+                    </b>
+                  </div>
+                </div>
+                <hr />
+                <div class="a-row">
+                  <b>
+                    Already have an account?
+                    <nuxt-link to="/login" class="a-link-emphasis"
+                      >Sign In</nuxt-link
+                    >
+                  </b>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "none"
+};
+</script>
