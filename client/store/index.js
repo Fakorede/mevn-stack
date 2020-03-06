86c1fa28e23bedd26565c1424426ac5dfb5eaca0
fakorede <abiolafakorede@gmail.com>
2020-03-06 15:15:56 +0100

    implementing state mngt with vuex

---
+++ client/store/index.js
@@ -0,0 +1,40 @@
+export const state = () => ({
+  cart: [],
+  cartLength: 0
+});
+
+export const actions = {
+  addProductToCart({ state, commit }, product) {
+    const cartProduct = state.cart.find(prod => prod._id === product._id);
+
+    if (!cartProduct) {
+      commit("pushProductToCart", product);
+    } else {
+      commit("incrementProductQty", product);
+    }
+
+    commit("incrementCartLength");
+  }
+};
+
+export const mutations = {
+  pushProductToCart(state, product) {
+    product.quantity = 1;
+    state.cart.push(product);
+  },
+
+  incrementProductQty(state, product) {
+    product.quantity++;
+    let indexOfProduct = state.cart.indexOf(product);
+    state.cart.splice(indexOfProduct, 1, product);
+  },
+
+  incrementCartLength(state) {
+    state.cartLength = 0;
+    if (state.cart.length > 0) {
+      state.cart.map(product => {
+        state.cartLength += product.quantity;
+      });
+    }
+  }
+};
