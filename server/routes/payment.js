6460aebb66af485a6cc54f69c634385b26eb32df
fakorede <abiolafakorede@gmail.com>
2020-03-07 14:08:39 +0100

    payment api

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 const moment = require("moment");
+const stripe = require("stripe");
+
+const verifyToken = require("../middlewares/verify-token");
+const Order = require("../models/order");
 
 const SHIPMENT = {
   regular: {
@@ -45,6 +49,51 @@ router.post("/shipment", async (req, res) => {
       message: err.message
     });
   }
+});
+
+router.post("/payment", verifyToken, (req, res) => {
+  let totalPrice = Math.round(req.body.totalPrice * 100);
+
+  // create new customer and charge for that customer
+  stripe.customers
+    .create({
+      email: req.decoded.email
+    })
+    .then(customer => {
+      return stripe.customers.createSource(customer.id, {
+        source: "tok_visa"
+      });
+    })
+    .then(source => {
+      return stripe.charges.create({
+        amount: totalPrice,
+        currency: "usd",
+        customer: source.customer
+      });
+    })
+    .then(async charge => {
+      // store order information
+      let order = new Order();
+      let cart = req.body.cart;
+
+      cart.map(product => {
+        order.products.push({
+          productID: product._id,
+          quantity: parseInt(product.quantity),
+          price: product.price
+        });
+      });
+
+      order.owner = req.decoded._id;
+      order.estimatedDelivery = req.body.estimatedDelivery;
+      await order.save();
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        message: err.message
+      });
+    });
 });
 
 module.exports = router;
