358beced041b64257ecd239c1f2fe09cd363c822
fakorede <abiolafakorede@gmail.com>
2020-03-07 18:49:20 +0100

    order page axios call

--- client/pages/orders.vue
+++ client/pages/orders.vue
@@ -6,7 +6,9 @@
       <div class="row">
         <div class="col-xl-2 col-lg-1 col-md-12"></div>
         <div class="col-xl-8 col-lg-9 col-md-12">
-          <div class="a-spacing-large a-spacing-top-small a-subheader a-breadcrumb">
+          <div
+            class="a-spacing-large a-spacing-top-small a-subheader a-breadcrumb"
+          >
             <ul class="a-unordered-list a-nostyle a-horizontal">
               <li>
                 <span class="a-list-item">
@@ -44,18 +46,26 @@
           <div class="orderContent">
             <div class="orderContentHeader">
               <div class="row">
-                <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 text-left">
+                <div
+                  class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2 text-left"
+                >
                   <div class="a-row">
-                    <span class="a-size-mini a-color-secondary">ORDER PLACED</span>
+                    <span class="a-size-mini a-color-secondary"
+                      >ORDER PLACED</span
+                    >
                     <br />
-                    <span class="a-size-base a-text-bold a-color-secondary">May 16, 2016</span>
+                    <span class="a-size-base a-text-bold a-color-secondary"
+                      >May 16, 2016</span
+                    >
                   </div>
                 </div>
                 <div class="col-xl-2 col-lg-2 col-md-2 col-sm-2 col-2">
                   <div class="a-row">
                     <span class="a-size-mini a-color-secondary">TOTAL</span>
                     <br />
-                    <span class="a-size-base a-text-bold a-color-secondary">$904.85</span>
+                    <span class="a-size-base a-text-bold a-color-secondary"
+                      >$904.85</span
+                    >
                   </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-3">
@@ -64,63 +74,82 @@
                     <br />
                     <!-- Owner's name -->
                     <a href class="a-size-base font-weight-bold a-link-normal">
-                      Jack
+                      {{ $auth.$state.user.name }}
                       <i class="far fa-chevron-down"></i>
                     </a>
                   </div>
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-5 text-right">
+                <div
+                  class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-5 text-right"
+                >
                   <div class="a-row">
-                    <span class="a-size-mini a-color-secondary">ORDER # 114-7570830-75444212</span>
+                    <span class="a-size-mini a-color-secondary"
+                      >ORDER # 114-7570830-75444212</span
+                    >
                     <br />
-                    <a href="#" class="a-size-base font-weight-bold a-link-normal">Order Details</a>
+                    <a
+                      href="#"
+                      class="a-size-base font-weight-bold a-link-normal"
+                      >Order Details</a
+                    >
                     &nbsp;
                     <div
                       style="display: inline-block; background-color: #ddd; height: 15px; width: 2px; margin-bottom: -3px;"
-                    ></div>&nbsp;
-                    <a href="#" class="a-size-base font-weight-bold a-link-normal">Invoice</a>
+                    ></div>
+                    &nbsp;
+                    <a
+                      href="#"
+                      class="a-size-base font-weight-bold a-link-normal"
+                      >Invoice</a
+                    >
                   </div>
                 </div>
               </div>
             </div>
             <!-- Orders body -->
-            <div class="orderContentBodyAlt">
+            <div class="orderContentBodyAlt" v-for="order in orders" :key="order._id">
               <div class="a-row">
                 <h1
                   class="a-size-medium a-text-bold"
                   style="color: #111 !important;font-family: 'MyWebFont',Arial,sans-serif !important; "
                 >
                   <!-- Estimated Delivery -->
-                  Estimated Delivery -
+                  Estimated Delivery - {{ order.estimatedDelivery }}
                 </h1>
               </div>
               <!-- List of products from order -->
-              <div>
+              <div v-for="product in order.products" :key="product._id">
                 <div class="a-spacing-base"></div>
                 <div class="row">
                   <div class="col-xl-2 col-lg-2 col-2">
                     <!-- Product's image -->
                     <a href="#">
-                      <img class="img-fluid" style="width: 100px;" />
+                      <img :src="product.productID.photo" class="img-fluid" style="width: 100px;" />
                     </a>
                   </div>
                   <div class="col-xl-10 col-lg-10 col-10">
                     <div class="a-row">
                       <span class="a-size-small">
                         <!-- Product title -->
-                        <a href="#">Title</a>
+                        <a href="#">{{ product.productID.title }}</a>
                       </span>
                     </div>
                     <div class="a-row">
-                      <span class="a-size-mini a-color-secondary">Sold by: Amazon Export Sales LLC</span>
+                      <span class="a-size-mini a-color-secondary"
+                        >Sold by: Amazon Export Sales LLC</span
+                      >
                     </div>
                     <div class="a-row">
                       <!-- Product quantity -->
-                      <span class="a-size-mini" style="color: #111; font-weight: 400;">Quantity: 3</span>
+                      <span
+                        class="a-size-mini"
+                        style="color: #111; font-weight: 400;"
+                        >Quantity: {{ product.productID.quantity }}</span
+                      >
                     </div>
                     <div class="a-row">
                       <!-- Product price -->
-                      <span class="a-size-mini a-color-price">$99</span>
+                      <span class="a-size-mini a-color-price">${{ product.productID.price }}</span>
                     </div>
                     <br />
                     <div class="a-row">
@@ -143,7 +172,17 @@
 
 <script>
 export default {
-    
-}
-</script>
+  async asyncData({ $axios }) {
+    try {
+      let response = await $axios.$get("/api/v1/orders");
 
+      return {
+        orders: response.products
+      };
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  data() {}
+};
+</script>
